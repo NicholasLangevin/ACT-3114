965fa4ea78ff1606d209d9ba44b7b591b6e22a6f
Nicholas Langevin <Langevin.Nicholas@hotmail.com>
2020-03-30 12:16:51 -0400

    corect. datasets

--- script/00c-TrainValidTest.R
+++ script/00c-TrainValidTest.R
@@ -9,10 +9,11 @@ pourcent_test <- 0.2
 ## Creation du dataset train
 index_trainData <- sample(1:nrow(data), size = floor(pourcent_train * nrow(data)))
 trainData <- data[index_trainData, ]
-testData <- data[!index_trainData, ]
+testData <- data[-index_trainData, ]
 
 if (nrow(data) == nrow(trainData) + nrow(testData))
 {
     save(trainData, file="../data/trainData.RData")
     save(testData,  file="../data/testData.RData")
+    print("Saved")
 }
