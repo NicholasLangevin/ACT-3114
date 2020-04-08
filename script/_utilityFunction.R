1f9233a985d5d989c09231b89ba4ced637c8a4fa
Nicholas Langevin <Langevin.Nicholas@hotmail.com>
2020-04-08 19:45:24 -0400

    first draft cv

--- script/_utilityFunction.R
+++ script/_utilityFunction.R
@@ -46,3 +46,97 @@ ROC <- function(y, probPred, add = FALSE, ...){
         abline(a=0, b=1)
     }
 }
+
+Rmd_table <- function(dat, caption="", label="", 
+                         align=rep("c", ncol(dat)+1), digits=3, ...){
+ 
+    require(xtable)
+    print(xtable(dat, digits = digits,
+                 caption=caption,
+                 label=label,
+                 align=align),
+                 include.rownames = FALSE, 
+                 sanitize.text.function=function(x){x},
+                 caption.placement = "top", comment=FALSE, ...)
+}
+
+
+
+# grid <- expand.grid(cp=0.0, minbucket=1)
+# args <- list(formula=lapse~., method = "class", cp=0.0, minbucket=1)
+# FUN <- "rpart"
+# data <- testData
+# nfold <- 2
+
+vc <- function(nfold, data, FUN, args){
+    if (nfold <= 1) stop("Argument 'nfold' need to be as possite integer")
+    require(pROC)
+    n <- nrow(data)
+    taille <- n %/% nfold
+    set.seed(1001)
+    alea <- runif(n)
+    rang <- rank(alea)
+    summary(rang)
+    block <- (rang-1) %/% taille + 1
+    block_tmp <- as.factor(block)
+    block <- ifelse(block > as.numeric(levels(block_tmp)[nfold]), nfold, block)
+    block <- as.factor(block)
+    
+    auc <- numeric(nfold)
+    for (fold in seq(nfold)){
+
+        data.args <- list(data=data[block != fold,])
+        mod <- do.call(eval(parse(text=FUN)), c(data.args, args))
+
+        if (FUN == "knn") 
+            pred <- mod$pred
+        else
+            pred <- predict(mod, data[block == fold,], type="prob")
+
+        auc[fold] <- as.numeric(suppressMessages(roc(dataToNumeric(data[block == fold,], "y"), pred[, 2]))$auc)
+
+    }
+    return(auc)
+}
+
+gridSearch <- function(grid, nfold, data, FUN, args){
+
+    grid.n <- nrow(grid)
+    grid.colnames <- colnames(grid)
+    fold.mean <- numeric(grid.n)
+    fold.sd <- numeric(grid.n)
+    percByFold <- 1/grid.n
+    perc <- 0
+    for (row in seq(grid.n)){
+
+        message(scales::percent(perc, suffix =" %", accuracy = 0.01))
+
+        grid.row <- grid[row,]
+        params <- lapply(grid.row, function(row) row)
+        mod.args <- c(args, params)
+        call.args <- list(nfold=nfold,
+                         FUN = FUN,
+                         data = data,
+                         args=mod.args)
+
+
+        val <- do.call(vc, call.args)
+        fold.mean[row] <- mean(val)
+        fold.sd[row] <- sd(val)
+
+        perc <- perc + percByFold
+
+    }
+
+    grid$AUCmean <- fold.mean
+    grid$AUCsd <- fold.sd
+    return(grid)
+}
+
+        
+
+
+
+
+
+
