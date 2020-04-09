890f90dcd9030c0d026f886381f78cd4bba65999
Charles Comeau <charlescomeau02@gmail.com>
2020-04-08 20:06:11 -0400

    Bagging et Forest terminé

--- script/05-Bagging.R
+++ script/05-Bagging.R
@@ -24,7 +24,7 @@ mod_bag <- randomForest(lapse ~ ., data = trainData,
                         keep.forest = TRUE)
 
 plot(1:dim(mod_bag$err.rate)[1], mod_bag$err.rate[,1],
-     type="l", xlab="B", ylab="Taux d'erreur OOB", main="Bagging") ## combien d'arbres ? -> stabilisation a 150 arbres
+     type="l", xlab="B", ylab="Taux d'erreur OOB", main="Bagging") ## combien d'arbres ? -> stabilisation a 100 arbres
 
 varImpPlot(mod_bag) ## visualiser l'importance des variables 
 
@@ -60,7 +60,7 @@ for(i in 1:4){
 # sort(c(as.numeric(rownames(train_cv_data[[1]])), as.numeric(rownames(valid_cv_data[[1]]))))
 
 
-res_opt <- matrix(nrow = 49, ncol = 2)
+res_opt_bag <- matrix(nrow = 49, ncol = 2)
 j <- 1
 for(node in 2:50){
     auc_opt <- numeric(4)
@@ -77,11 +77,12 @@ for(node in 2:50){
         auc_k <- as.numeric(roc(valid_cv_data[[k]]$lapse, preds.bag[,2], auc=TRUE, plot=FALSE, quiet = TRUE)$auc)
         auc_opt[k] <- auc_k
     }
-    res_opt[j, ] <- c(node, mean(auc_opt))
+    res_opt_bag[j, ] <- c(node, mean(auc_opt))
     j <- j + 1
 }
 
-nodesize_opti <- res_opt[which.max(res_opt[, 2])] # 34
+saveRDS(res_opt_bag, file = paste0(getwd(), "/src/05-Bagging/result_opt_bag_save.rds"))
+nodesize_opt <- res_opt_bag[which.max(res_opt_bag[, 2])] # 34
 
 
 ## Performance du bagging choisi (nodesize = 34)
@@ -89,67 +90,16 @@ set.seed(23654)
 mod_bag_final <- randomForest(lapse ~ ., data = trainData,
                               ntree = 150,
                               sampsize= nrow(trainData),
-                              nodesize = nodesize_opti,
+                              nodesize = nodesize_opt,
                               mtry = ncol(trainData[, -1]),
                               cp = 0,
                               importance = TRUE,
                               keep.forest = TRUE)
 
+saveRDS(mod_bag_final, file = paste0(getwd(), "/src/05-Bagging/mod_bagging.rds"))
+
 preds.bag_final <- predict(mod_bag_final, newdata = testData, type="prob")
 roc(testData$lapse, preds.bag_final[,2], auc=TRUE, plot=TRUE)
-
-
-### Optimisation de l'hyperparamètres nodesize  (#### cette méthode fonctionnerait mais il faudrait une échantillon de validation...)
-# res_opt <- matrix(nrow = 29 *10, ncol = 3)
-# cp_test <- seq(0.0001, 0.001, length.out = 10)
-# j <- 1
-# for(node in 2:30){
-#     for(cp in cp_test){
-#         mod_bag_opt <- randomForest(lapse~., data = trainData, ntree = 100,
-#                                     nodesize = node, cp = 0, importance = TRUE, keep.forest = TRUE)
-#         preds.bag <- predict(mod_bag, newdata = testData, type="prob")
-#         auc <- as.numeric(roc(testData$lapse, preds.bag[,2], auc=TRUE, plot=TRUE, quiet = TRUE)$auc)
-#         res_opt[j, ] <- c(node, cp, auc)
-#         j <- j + 1
-#     }
-# }
-# 
-# 
-# fit.control_cv <- trainControl(method="cv",
-#                                number=5,
-#                                summaryFunction = twoClassSummary,
-#                                classProbs=TRUE)
-# 
-# cv.fitted.bag_cv <- train(lapse ~ .,
-#                            data = trainData,
-#                            method = "AdaBag",
-#                            tuneGrid = expand.grid(maxdepth = c(2:30), mfinal = 200),
-#                            metric = "ROC",
-#                            trControl = fit.control_cv)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- script/06-RandomForest.R
+++ script/06-RandomForest.R
@@ -12,24 +12,34 @@ require(pROC)
 require(Rborist)
 
 set.seed(3876)
-mod_foret_pre <- randomForest(lapse ~ ., data=trainData, ntree = 200, sampsize = floor(0.75*nrow(trainData)),
-                          nodesize = 2, cp = cp_optimal, mtry = 2, importance = TRUE, keep.forest = TRUE)
+mod_forest_pre <- randomForest(lapse ~ ., data=trainData,
+                              ntree = 300, 
+                              sampsize = floor(0.5*nrow(trainData)),
+                              nodesize = 2,
+                              cp = 0,
+                              mtry = 4, # sqrt(19), 19 etant le nombre de params max 
+                              importance = TRUE,
+                              keep.forest = TRUE)
 
 
-plot(1:dim(mod_foret_pre$err.rate)[1], mod_foret_pre$err.rate[,1],
-     type="l", xlab="B", ylab="Taux d'erreur OOB", main="Foret") ## combien d'arbres ? -> 100 suffira largement
+plot(1:dim(mod_forest_pre$err.rate)[1], mod_foret_pre$err.rate[,1],
+     type="l", xlab="B", ylab="Taux d'erreur OOB", main="Foret") ## combien d'arbres ? -> stabilisation a 100 arbres
 
+## Performance du modele de base 
+preds.forest <- predict(mod_forest_pre, newdata = testData, type="prob")
+roc(testData$lapse, preds.forest[,2], auc=TRUE, plot=TRUE)
 
 ## Optimisation par validation croisée
-control <- trainControl(method='cv', 
-                        number=3,
+control <- trainControl(method = 'cv', 
+                        number = 5,
                         summaryFunction = twoClassSummary,
-                        classProbs=TRUE)
+                        classProbs = TRUE)
 
 ## mtry  et nodesize optimal ?  Attention c'est quand meme long
-res_opt <- matrix(nrow = 29, ncol = 3)
+res_opt <- matrix(nrow = 8, ncol = 3)
 set.seed(4414)
-for(node in 2:30){
+j <- 1
+for(node in c(1:8 * 5)){
     rf_tuning <- train(lapse ~ .,
                        data = trainData,
                        method = 'rf',
@@ -37,34 +47,35 @@ for(node in 2:30){
                        tuneGrid = expand.grid(mtry = 2:15), 
                        trControl = control,
                        sampsize = floor(0.5*nrow(trainData)),
-                       ntree = 50,
+                       ntree = 100,
                        nodesize = node)
     mtry_opti <- rf_tuning$results$mtry[which.max(rf_tuning$results$ROC)]
     AUC_opti <- rf_tuning$results$ROC[which.max(rf_tuning$results$ROC)]
-    res_opt[node - 1, ] <- c(node, mtry_opti, AUC_opti)
+    
+    res_opt[j, ] <- c(node, mtry_opti, AUC_opti)
+    j <- j + 1
 }
-
 node_opt <- res_opt[which.max(res_opt[, 3]), ][1]
 mtry_opt <- res_opt[which.max(res_opt[, 3]), ][2]
-
-mod_foret_fin <- randomForest(lapse ~ ., data=trainData, ntree = 100, sampsize = floor(0.50*nrow(trainData)),
-                              nodesize = node_opt, cp = 0, mtry = mtry_opt, importance = TRUE, keep.forest = TRUE)
-
-preds.foret <- predict(mod_foret_fin, newdata = testData, type="prob")
-roc(testData$lapse, preds.foret[,2], auc=TRUE, plot=TRUE)
+saveRDS(res_opt, file = paste0(getwd(), "/src/06-RandomForest/result_opt_rf_save.rds"))
 
 
+res_opt_save <- readRDS(paste0(getwd(), "/src/06-RandomForest/result_opt_rf_save.rds"))
 
+## Performance de la forest choisie
+set.seed(45)
+mod_forest_fin <- randomForest(lapse ~ ., data = trainData,
+                              ntree = 100,
+                              sampsize = floor(0.5*nrow(trainData)),
+                              nodesize = node_opt,
+                              cp = 0,
+                              mtry = mtry_opt,
+                              importance = TRUE,
+                              keep.forest = TRUE)
 
+saveRDS(mod_forest_fin, file = paste0(getwd(), "/src/06-RandomForest/mod_forest.rds"))
 
-
-
-
-
-
-
-
-
-
+preds.forest_fin <- predict(mod_forest_fin, newdata = testData, type="prob")
+roc(testData$lapse, preds.forest_fin[,2], auc=TRUE, plot=TRUE)
 
 
