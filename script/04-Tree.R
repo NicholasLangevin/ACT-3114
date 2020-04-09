e86033290e32157152fadd90126696c25c709a63
Nicholas Langevin <Langevin.Nicholas@hotmail.com>
2020-04-09 02:43:52 -0400

    Fin rapport tree

--- script/04-Tree.R
+++ script/04-Tree.R
@@ -5,36 +5,44 @@ library(rpart)
 library(rpart.plot)
 library(pROC)
 library(caret)
+source("../script/_utilityFunction.R")
 
 ## Import data
 load("../data/trainData.RData")
 load("../data/testData.RData")
 
+
+
+## Arbre complet T0
+tree.T0 <- rpart(lapse~., 
+              data      = trainData,
+              method    = "class",
+              control   = tree.control,
+              minbucket = 1,
+              cp        = 0.0)
+plotcp(tree.T0, lty=2, col=2)
+summary(tree.T0)
+save(tree.T0, file="../src/04-tree/tree.T0.rds")
+
+
 ## Optimisation des Hyperparametres
-trControl.tree <- trainControl(method="cv", # Cross-Validation
-                               number=4, # Nombre de plis
-                               summaryFunction = twoClassSummary,
-                               classProbs=TRUE,
-                               #Methode de gestion du debalancement des donnees
-                               sampling = "smote"
-                               ) 
-# TODO: Utilise autre chose que l indice de gini
-gridSearch.rpart <- function(maxdepth){
-    nResult <- length(maxdepth)
-    gridSearchResults <- data.frame(maxdepth = maxdepth,
-                                    cp=maxdepth,
-                                    ROC=maxdepth)
+gridSearch.rpart <- function(minbucket, split="gini"){
+
+    nResult <- length(minbucket)
+    gridSearchResults <- data.frame(minbucket = minbucket,
+                                    cp=minbucket,
+                                    ROC=minbucket)
     iter <- 1
     # sapply(maxdepth, function(depth){
-    for(depth in maxdepth){
+    for(bucket in minbucket){
         fit.tree <- train(lapse ~ .,
                           method     = "rpart",
                           preProcess = "scale",
-                          control=rpart.control(maxdepth = depth,
-                                                minbucket=1),
+                          control=rpart.control(minbucket = bucket),
                           tuneLength = 20,
                           trControl  = trControl.tree,
                           metric     = "ROC",
+                          parms=list(split=split),
                           data       = trainData
                           )
         gridSearchResults$cp[iter] <- as.numeric(fit.tree$bestTune)
@@ -46,61 +54,37 @@ gridSearch.rpart <- function(maxdepth){
     return(gridSearchResults)
 }
 
-## Les resultat changent quelques fois mais maxdepth=6 gagne souvent
-(gridResult <- gridSearch.rpart(5:12))
-gridResult
+trControl.tree <- trainControl(method="cv", # Cross-Validation
+                               number=4, # Nombre de plis
+                               summaryFunction = twoClassSummary,
+                               classProbs=TRUE)
 
+(tree.gridResult.gini <- gridSearch.rpart(10:20, "gini"))
+(tree.gridResult.info <- gridSearch.rpart(10:20, "information"))
+
+save(tree.gridResult.gini, file="../src/04-tree/tree.gridResult.gini.rds")
+save(tree.gridResult.info, file="../src/04-tree/tree.gridResult.info.rds")
+
+
+
+load(file="../src/04-tree/tree.gridResult.gini.rds")
+gridResult <- tree.gridResult.gini
 bestModel <- which.max(gridResult$ROC)
 (cp <- gridResult$cp[bestModel])
-(maxdepth <- gridResult$maxdepth[bestModel])
+(minbucket <- gridResult$minbucket[bestModel])
 
 ## Entrainement du meilleur model
-tree.control <- rpart.control(cp=0.0, 
-                              minbucket=1,
-                              maxdepth=maxdepth
-                              )
 tree <- rpart(lapse~., 
               data=trainData,
               method="class",
-              control=tree.control
-              )
+              cp=0.0,
+              minbucket=minbucket)
 
-
-plotcp(tree)
-# Selon cette métrique, l’arbre racine est le meilleur.
-
-source("_utilityFunction.R")
-treeCutted <- prune(tree, cp=cp)
-rpart.plot(treeCutted, type=0, fallen.leaves = FALSE, tweak=2.2)
-rpart.plot(tree, type=0, fallen.leaves = FALSE, tweak=2.2)
+tree.final <- prune(tree, cp=cp)
+rpart.plot(tree.final, type=0, fallen.leaves = FALSE, tweak=2.2)
 
 ## Test sur les donnees test
-pred <- predict(treeCutted, newdata = testData)
+pred <- predict(tree.final, newdata = testData)
 roc <- roc(dataToNumeric(testData, "y"), pred[,2], plot=TRUE)
+# ROC(dataToNumeric(testData, "y"), pred[,2])
 as.numeric(roc$auc)
-
-
-(grid <- expand.grid(cp=seq(0.0001, 0.0005, length.out=20), maxdepth=5:7))
-(res <- gridSearch.rpart2(grid))
-
-gridSearch.rpart2 <- function(grid){
-    gridSearchResults <- data.frame(maxdepth = numeric(nrow(grid)),
-                                    cp=numeric(nrow(grid)),
-                                    ROC=numeric(nrow(grid)))
-    for(i in 1:nrow(grid)){
-        tree <- rpart(lapse~., 
-                      data=trainData,
-                      method="class",
-                      cp=grid$cp[i],
-                      maxdepth=grid$maxdepth[i]
-                      )
-        pred <- predict(tree, newdata=testData)
-        gridSearchResults$maxdepth[i] = grid$maxdepth[i]
-        gridSearchResults$cp[i] = grid$cp[i]
-        gridSearchResults$ROC[i] = as.numeric(roc(dataToNumeric(testData, "y"),
-                                                  pred[,2])$auc)
-        print(as.numeric(roc(dataToNumeric(testData, "y"), pred[,2])$auc))
-        #
-    }
-    return(gridSearchResults)
-}
