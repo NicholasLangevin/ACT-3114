6dfc90464f4d1d2c2371b1e14ba6fb8a2a458127
AndreanneLarouche <andreannelarouche@outlook.fr>
2020-04-20 16:22:14 -0400

    Ajout de tous les graphiques pour l'interpértation et modif de la prob de prévision pour gbm

--- script/07-gbm.R
+++ script/07-gbm.R
@@ -13,6 +13,7 @@ load("../data/trainData.RData")
 load("../data/testData.RData")
 trainDataBool <- trainData
 testDataBool <- testData
+trainData
 
 ## GBM Bernoulli utilisant ROC pour entrainer le modèle
 controles_Bern <- trainControl(method="cv", number = 4,
@@ -40,8 +41,8 @@ gbmfit_Bern$bestTune   # paramétres optimaux
 plot(gbmfit_Bern)      # visualisation de l'optimisation
 
 ## Pour que la variable réponse prenne les valeurs 0, 1.
-trainDataBool$lapse <- trainData$lapse == "renouvellement"
-testDataBool$lapse <- testData$lapse == "renouvellement"
+trainDataBool$lapse <- trainData$lapse == "resignation"
+testDataBool$lapse <- testData$lapse == "resignation"
 
 set.seed(2000)
 #gbm_opti_Bern <- gbm(lapse ~ ., 
@@ -65,8 +66,8 @@ previsions_Bern <- predict(gbm_opti_Bern,
 ## AUC et courbe ROC
 roc_gbm <- roc(testDataBool$lapse, previsions_Bern)
 auc <- as.numeric(roc(testDataBool$lapse, previsions_Bern)$auc)
-save(roc_gbm,file="../src/07-gbm/roc.rds")
-save(auc,file="../src/07-gbm/auc.rds")
+#save(roc_gbm,file="../src/07-gbm/roc.rds")
+#save(auc,file="../src/07-gbm/auc.rds")
 
 
 ##
@@ -80,7 +81,15 @@ save(auc,file="../src/07-gbm/auc.rds")
 ## nouvel échantillon. (25% des données initiales)
 
 ## Importance des variables
-summary(gbm_opti_Bern, las = 1)
+imp <- summary(gbm_opti_Bern, las = 1, plot=TRUE)
+variables <- rownames(imp)
+rel.inf <- imp[2]
+data <- data.frame(variables, rel.inf)
+ggplot(data, aes(x = reorder(variables, rel.inf), y=rel.inf)) + 
+  geom_bar(stat = 'identity') + 
+  coord_flip() +
+  theme_bw() +
+  labs(x="variable explicative", y="Variance relative sur la prévision (%)")
 
 
 ## PDP : Graphiquede dépendance partielle
@@ -88,18 +97,45 @@ summary(gbm_opti_Bern, las = 1)
 ##
 ## Peut être interprété comme la moyenne des ICEs (ce n'est pas parfait comme méthoe
 ## car il y a beaucoup de courbe superposées)
-plot(gbm_opti_Bern, 1, gbmfit_Bern$bestTune["n.trees"], type ="response")
+pdp_prem_index <- plot(gbm_opti_Bern, 19, gbmfit_Bern$bestTune["n.trees"], type ="response") #prem_index
+pdp_vehicl_region <- plot(gbm_opti_Bern, 18, gbmfit_Bern$bestTune["n.trees"], type ="response") #vehicl_region
+pdp_policy_age <- plot(gbm_opti_Bern, 6, gbmfit_Bern$bestTune["n.trees"], type ="response") #policy_age
+#save(pdp_prem_index, file = "../src/07-gbm/pdp_prem_index.rds")
+#save(pdp_vehicl_region, file = "../src/07-gbm/pdp_vehicl_region.rds")
+#save(pdp_policy_age, file = "../src/07-gbm/pdp_policy_age.rds")
 
+
+#plot(gbm_opti_Bern, 10, gbmfit_Bern$bestTune["n.trees"], type ="response") #prem_freqperyear
+#plot(gbm_opti_Bern, 1, gbmfit_Bern$bestTune["n.trees"], type ="response") #polholder_age
+#plot(gbm_opti_Bern, 14, gbmfit_Bern$bestTune["n.trees"], type ="response") #vehicl_age
+#plot(gbm_opti_Bern, 11, gbmfit_Bern$bestTune["n.trees"], type ="response") #prem_last
+#plot(gbm_opti_Bern, 15, gbmfit_Bern$bestTune["n.trees"], type ="response") #vehicl_agepurchase
+#plot(gbm_opti_Bern, 13, gbmfit_Bern$bestTune["n.trees"], type ="response") #prem_pure
+#plot(gbm_opti_Bern, 12, gbmfit_Bern$bestTune["n.trees"], type ="response") #prem_market
+#plot(gbm_opti_Bern, 3, gbmfit_Bern$bestTune["n.trees"], type ="response") #polholder_diffdriver
+#plot(gbm_opti_Bern, 9, gbmfit_Bern$bestTune["n.trees"], type ="response") #prem_final
+#plot(gbm_opti_Bern, 16, gbmfit_Bern$bestTune["n.trees"], type ="response") #vehicl_garage
+#plot(gbm_opti_Bern, 4, gbmfit_Bern$bestTune["n.trees"], type ="response") #polholder_gender
+#plot(gbm_opti_Bern, 5, gbmfit_Bern$bestTune["n.trees"], type ="response") #polholder_job
+#plot(gbm_opti_Bern, 8, gbmfit_Bern$bestTune["n.trees"], type ="response") #policy_nbcontract
+#plot(gbm_opti_Bern, 17, gbmfit_Bern$bestTune["n.trees"], type ="response") #vehicl_poserkw
 
 ##Statistique H de Friedman
 ## On peut regarder quelles sont les variables qui interragissent avec prim_last et
 ## vehicl_region, soit les 2 variables les plus importantes pour expliquer lapse
 ## La statistique de Friedman permet d'estimer la force d'une interaction en mesurant la quantité
 ## de la variance dans la prévision qui provient de l'interraction
-set.seed(12345)
-mod.iml <- Predictor$new(gbmfit_Bern) #Créer un modèle utilisable avec iml
-int.prim_last <- Interaction$new(mod.iml, "prim_last")
-int.vehicl_region <- Interaction$new(mod.iml, "vehicl_region")
-plot(int.prim_last)
+
+#set.seed(12345)
+#mod.iml <- Predictor$new(gbmfit_Bern) #Créer un modèle utilisable avec iml
+#int.prem_index <- Interaction$new(mod.iml, "prem_index")
+#int.vehicl_region <- Interaction$new(mod.iml, "vehicl_region")
+#save(int.prem_index, file = "../src/07-gbm/int_prem_index.rds")
+#save(int.vehicl_region, file = "../src/07-gbm/int_vehicl_region.rds")
+load(file = "../src/07-gbm/int_prem_index.rds")
+load(file = "../src/07-gbm/int_vehicl_region.rds")
+plot(int.prem_index)
 plot(int.vehicl_region)
 
+##On rajoute ensuite 1/2 graphiques de pdp sur les interractions les plus importantes
+##PARTIE QUI RESTE À FAIRE
